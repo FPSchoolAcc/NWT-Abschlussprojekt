8dbb4171df24429ec31104b95b93956fe845dd3c
Paul <94569573+FPSchoolAcc@users.noreply.github.com>
2022-06-25 12:17:11 +0200

    Create mv_follow.cpp

---
+++ Movement/mv_follow.cpp
@@ -0,0 +1,28 @@
+/*
+Date: 06.22.2022
+Students:
+Name of the project: Bluetooth-controlled Car
+independent following of an object
+*/
+
+//setup
+int followDistance = 10;
+...
+
+//loop
+...
+
+//def
+void Follow() {
+  digitalWrite(sendSignal, LOW);
+	delay(5);
+	digitalWrite(sendSignal, HIGH);
+	delay(5);
+	digitalWrite(sendSignal, LOW);
+	delay = pulseIN(getSignal, HIGH);
+	distance = (delay / 2) * 0.03432;
+
+  if (distance > followDistance) {
+    moveForward();
+  }
+}
