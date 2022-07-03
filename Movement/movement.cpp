737e6de95cde69073160f7e8fb26120d31dcd942
Paul <94569573+FPSchoolAcc@users.noreply.github.com>
2022-07-03 19:59:59 +0200

    Update movement.cpp

--- Movement/movement.cpp
+++ Movement/movement.cpp
@@ -1,42 +1,43 @@
-const int IN_A0 = A0;
-const int IN_D0 = 11;
+const int IN_A0 = A0;                                 //legt den analogen Pin für den IR-Sensor fest  
+const int IN_D0 = 11;                                 //legt den digitalen Pin für den IR-Sensor fest
 
-const int lGes = 10;
-const int lM1 = 9;
-const int lM2 = 8;
+const int lGes = 10;                                  //legt den PWM-Pin für die Geschwindigkeit der Motoren fest
+const int lM1 = 9;                                    //legt die Pins für die Ansteuerung des Motors fest
+const int lM2 = 8;                                    //legt die Pins für die Ansteuerung des Motors fest
 
-const int rGes = 5;
-const int rM1 = 7;
-const int rM2 = 6;
+const int rGes = 5;                                   //legt den PWM-Pin für die Geschwindigkeit der Motoren fest
+const int rM1 = 7;                                    //legt die Pins für die Ansteuerung des Motors fest
+const int rM2 = 6;                                    //legt die Pins für die Ansteuerung des Motors fest
 
-bool value_D0;
+bool value_D0;                                        //führt die Variable ein, in welocher gespeichert wird, ob eine Fahrbahnmarkierung erkannt wurde
 
 
 void setup() {
-  pinMode(lGes, OUTPUT);
-  pinMode(rGes, OUTPUT);
-  pinMode(lM1, OUTPUT);
-  pinMode(lM2, OUTPUT);
-  pinMode(rM1, OUTPUT);
-  pinMode(rM2, OUTPUT);
-  pinMode(IN_A0, INPUT);
-  pinMode(IN_D0, INPUT);
+  pinMode(lGes, OUTPUT);                              //an dem oben definierten Pin wird etwas augegeben
+  pinMode(rGes, OUTPUT);                              //an dem oben definierten Pin wird etwas augegeben
+  pinMode(lM1, OUTPUT);                               //an dem oben definierten Pin wird etwas augegeben
+  pinMode(lM2, OUTPUT);                               //an dem oben definierten Pin wird etwas augegeben
+  pinMode(rM1, OUTPUT);                               //an dem oben definierten Pin wird etwas augegeben
+  pinMode(rM2, OUTPUT);                               //an dem oben definierten Pin wird etwas augegeben
+  
+  pinMode(IN_A0, INPUT);                              //an dem oben definierten Pin wird eine Eingabe angenommen
+  pinMode(IN_D0, INPUT);                              //an dem oben definierten Pin wird eine Eingabe angenommen
 
-  Serial.begin(9600);
+  Serial.begin(9600);                                 //das Serial-Communication-Protocol wird gestartet
 }
 
 
 void loop () {
-  IrValue();
-  if (value_D0 == true) {
-    moveForeward();
+  IrValue();                                          //der IR-Sensor wird ausgelesen
+  if (value_D0 == true) {                             //falls der IR-Value-Wert zutrifft, dh. weißer Untergrund...
+    moveForeward();                                   //...fährt der wagen geradeaus
   }
-  else if (value_D0 == false) {
-    moveLeft();
+  else if (value_D0 == false) {                       //falls er jedoch nicht zutrifft, dh den Fahrbahnrand erkennt...
+    moveLeft();                                       //biegt er nach links ab
   }
 }
 
-void moveForeward () {
+void moveForeward () {                                //void für die Vorwärtsbewegung
   digitalWrite (lM1, HIGH);
   digitalWrite (lM2, LOW);
   analogWrite (lGes, 200);
@@ -45,7 +46,7 @@ void moveForeward () {
   analogWrite (rGes, 200);
 }
 
-void moveBackwads () {
+void moveBackwards () {                                //void für die Rückwertsbewegung
   digitalWrite (lM1, LOW);
   digitalWrite (lM2, HIGH);
   analogWrite (lGes, 200);
@@ -54,7 +55,7 @@ void moveBackwads () {
   analogWrite (rGes, 200);
 }
 
-void moveRight() {
+void moveRight() {                                     //void für die Rechtsbewegung
   digitalWrite (lM1, HIGH);
   digitalWrite (lM2, LOW);
   analogWrite (lGes, 200);
@@ -63,7 +64,7 @@ void moveRight() {
   analogWrite (rGes, 200);
 }
 
-void moveLeft () {
+void moveLeft () {                                    //void für die Linksbewegung
   digitalWrite (lM1, LOW);
   digitalWrite (lM2, HIGH);
   analogWrite (lGes, 200);
@@ -73,5 +74,5 @@ void moveLeft () {
 }
 
 void IrValue() {
-  value_D0 = digitalRead(IN_D0);
+  value_D0 = digitalRead(IN_D0);                      //der IR-Sensor wird ausgelesen
 }
